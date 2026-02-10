a2ee3a2bb1965687bee19e007c2ff00234cd42ee
65120020Chissanupong <65120020@kmitl.ac.th>
2026-02-10 19:59:49 +0700

    Update use while

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -2,7 +2,9 @@ print("Enter a number less than 25")
 number = int(input())
 
 if number <= 25:
-    for i in range(number, 26, 1):
+    i = number
+    while i <= 25:
         print(f"Inside the loop, my variable is {i}")
+        i += 1
 else:
     print("Error")
--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,6 +1,8 @@
 print("Enter a number: ")
 number = int(input())
 
-for i in range(0, 10):
+i = 0
+while i < 10:
     result = number * i
     print(f"{i} x {number} = {result}")
+    i += 1
