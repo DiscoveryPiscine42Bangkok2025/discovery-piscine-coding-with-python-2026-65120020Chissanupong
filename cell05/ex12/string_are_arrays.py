12d2a2e044372118fc27a5164e7cd0355197089a
65120020Chissanupong <65120020@kmitl.ac.th>
2026-02-11 22:39:37 +0700

    Update 5.12

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -6,11 +6,7 @@ if len(sys.argv) != 2:
     print("none")
 else:
     text = sys.argv[1]
-    count = 0
-
-    for char in text:
-        if char == 'z':
-            count += 1
+    count = text.count("z")
 
     if count == 0:
         print("none")
